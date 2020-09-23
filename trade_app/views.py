8a21a2a43325fee0ce404d490ad609c4dfa05a7b
Shay Brown <fire328@ymail.com>
2020-09-23 09:51:43 -0400

    add trending, movers and watchlists to context

--- trade_app/views.py
+++ trade_app/views.py
@@ -6,37 +6,81 @@ import requests
 
 
 def home(request):
+    # get news headlines
+    news_url = "https://bloomberg-market-and-financial-news.p.rapidapi.com/stories/list"
 
-    url = "https://bloomberg-market-and-financial-news.p.rapidapi.com/news/list"
+    news_query = {"template":"CURRENCY","id":"usdjpy"}
 
-    querystring = {"id":"stocks"}
-
-    headers = {
+    bloom_headers = {
         'x-rapidapi-host': "bloomberg-market-and-financial-news.p.rapidapi.com",
         'x-rapidapi-key': "3031e33fd9msh3c73fd1d3122a19p1a03abjsn0397c5598162"
     }
 
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    res_dict = response.json()
-    headline = res_dict['modules'][0]['stories']
-    # title = headline['title']
-    # summary = headline['summary']
-    # published = headline['published']
-    # short_url = headline['shortURL']
+    response = requests.request("GET", news_url, headers=bloom_headers, params=news_query)
+    news_dict = response.json()
+    headline = news_dict['stories'][0:4]
 
-    for d in headline:
-        dt = datetime.fromtimestamp(d['updatedAt']).strftime("%d %b %Y %H:%M:%S")
+    for h in headline:
+        dt = datetime.fromtimestamp(h['published']).strftime("%d %b %Y %H:%M:%S")
         dl = datetime.strptime(dt, "%d %b %Y %H:%M:%S")
-        d['updatedAt'] = dl
+        h['published'] = dl
 
     updated_now = datetime.now()
 
-    news = {
+    # get trending tickers
+
+    trend_url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-trending-tickers"
+
+    trend_query = {"region":"US"}
+
+    yahoo_headers = {
+        'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com",
+        'x-rapidapi-key': "3031e33fd9msh3c73fd1d3122a19p1a03abjsn0397c5598162"
+    }
+
+    response = requests.request("GET", trend_url, headers=yahoo_headers, params=trend_query)
+    trend_dict = response.json()
+    latest_trends = trend_dict['finance']['result'][0]['quotes'][0:4]
+
+    # get popular watchlists
+
+    pop_url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-popular-watchlists"
+
+    response = requests.request("GET", pop_url, headers=yahoo_headers)
+    pop_dict = response.json()
+    pop_watch = pop_dict['finance']['result'][0]['portfolios'][0:3]
+
+    for p in pop_watch:
+        p['followerCount'] = "{:,}".format(p['followerCount'])
+    
+    # get movers
+
+    movers_url = "https://morning-star.p.rapidapi.com/market/get-movers"
+
+    movers_query = {"PerformanceId":"0P00001GJH"}
+
+    morning_headers = {
+        'x-rapidapi-host': "morning-star.p.rapidapi.com",
+        'x-rapidapi-key': "3031e33fd9msh3c73fd1d3122a19p1a03abjsn0397c5598162"
+    }
+
+    response = requests.request("GET", movers_url, headers=morning_headers, params=movers_query)
+    movers_dict = response.json()
+    actives = movers_dict['Top10']['Actives']['Securities'][0:4]
+    gainers = movers_dict['Top10']['Gainers']['Securities'][0:4]
+    losers = movers_dict['Top10']['Losers']['Securities'][0:4]
+
+    content = {
         'news': headline,
-        'updated': updated_now
+        'updated': updated_now,
+        'actives': actives,
+        'gainers': gainers,
+        'losers': losers,
+        'trends': latest_trends,
+        'popular': pop_watch
     }
     
-    return render(request, 'base.html', news)
+    return render(request, 'base.html', content)
 
 def portfolio(request):
     url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-historical-data"
@@ -72,4 +116,10 @@ def portfolio(request):
 
 def trade(request):
     return render(request, 'trade.html')
+
+# def searchbar(request):
+#     if request.method =='GET':
+#         search = request.GET.get('search')
+#         post = trade_app.objects.all().filter(title=search)
+#         return render(request,)
 
