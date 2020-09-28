1697696d10da8f7ca66e3978be189e2d0625fb90
Shay Brown <fire328@ymail.com>
2020-09-28 18:22:33 -0400

    work on models, make migrations

--- trade_app/admin.py
+++ trade_app/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Account, Order, Watchlist, Position
 
 # Register your models here.
+
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('user', 'cash_balance')
+    list_filter = ('user',)
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('symbol', 'shares', 'paid', 'date', 'user')
+    list_filter = ('user', 'date')
+
+class WatchlistAdmin(admin.ModelAdmin):
+    list_display = ('symbol', 'name', 'user')
+    list_filter = ('user',)
+
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ('symbol', 'name', 'shares', 'user')
+    list_filter = ('user',)
+
+admin.site.register(Account, AccountAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Watchlist, WatchlistAdmin)
+admin.site.register(Position, PositionAdmin)
--- trade_app/models.py
+++ trade_app/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Account(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    cash_balance = models.FloatField(default=1500)
+
+    def __str__(self):
+        return self.user.username
+
+class Position(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    symbol = models.CharField(max_length=8)
+    name = models.CharField(max_length=100)
+    shares = models.IntegerField(default=0)
+
+    def __str__(self):
+        return "%s, %d" %  (self.symbol, self.shares)
+
+class Watchlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    symbol = models.CharField(max_length=8)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.symbol
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    symbol = models.CharField(max_length=8)
+    shares = models.IntegerField(default=0)
+    date = models.DateField('purchase date')
+    paid = models.FloatField()
+
+    def __str__(self):
+        return "%s, %d" %  (self.symbol, self.shares)
