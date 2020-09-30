21703a3d6ec34bf78c350ac03c3cc86dfbb17a4f
Shay Brown <fire328@ymail.com>
2020-09-30 00:44:11 -0400

    add paths for login, logout and password change;

--- trade_app/urls.py
+++ trade_app/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
+from .forms import LoginForm, PasswordChange
 
 urlpatterns = [
     path('', views.home, name="home"),
     path('portfolio/', views.portfolio, name="portfolio"),
     path('trade/', views.trade, name="trade"), 
-    path('login/', views.get_login, name="login"),
+    path('login/', LoginView.as_view(template_name='trade_app/login.html', authentication_form=LoginForm), name="login"),
+    path('login/user/', views.user_account, name="useract"),
+    path('logout/', LogoutView.as_view(template_name='trade_app/logout.html'), name="logout"),
     path('register/', views.get_registration, name="register"),
-    path('register/<str:newuser>/confirm/', views.deposit, name="confirm-user")
+    path('register/<str:newuser>/confirm/', views.deposit, name="confirm-user"),
+    path('password_change/', PasswordChangeView.as_view(template_name='trade_app/pwd-change.html', success_url='confirmed', form_class=PasswordChange), name="pwd-change"),
+    path('password_change/confirmed', views.new_pwd_confirmed, name="pwd_confirmed")
+
 ]
