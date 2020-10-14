32b1930d797aa7e48b92c51aac8d5e31f4066131
Shay Brown <fire328@ymail.com>
2020-10-14 00:27:48 -0400

    change models;

--- trade_app/admin.py
+++ trade_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Account, Order, Watchlist, Position
+from .models import Account, StockOrder, Watchlist, Position
 
 # Register your models here.
 
@@ -7,9 +7,10 @@ class AccountAdmin(admin.ModelAdmin):
     list_display = ('user', 'cash_balance')
     list_filter = ('user',)
 
-class OrderAdmin(admin.ModelAdmin):
-    list_display = ('symbol', 'shares', 'paid', 'date', 'user')
-    list_filter = ('user', 'date')
+class StockOrderAdmin(admin.ModelAdmin):
+    list_display = ('user', 'symbol', 'name', 'price', 'action', 'quantity', 'order_date')
+    list_filter = ('user',)
+
 
 class WatchlistAdmin(admin.ModelAdmin):
     list_display = ('symbol', 'name', 'user')
@@ -19,7 +20,8 @@ class PositionAdmin(admin.ModelAdmin):
     list_display = ('symbol', 'name', 'shares', 'user')
     list_filter = ('user',)
 
+
 admin.site.register(Account, AccountAdmin)
-admin.site.register(Order, OrderAdmin)
+admin.site.register(StockOrder, StockOrderAdmin)
 admin.site.register(Watchlist, WatchlistAdmin)
 admin.site.register(Position, PositionAdmin)
--- trade_app/models.py
+++ trade_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import date
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your models here.
 class Account(models.Model):
@@ -26,12 +28,14 @@ class Watchlist(models.Model):
     def __str__(self):
         return self.symbol
 
-class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+class StockOrder(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     symbol = models.CharField(max_length=8)
-    shares = models.IntegerField(default=0)
-    date = models.DateField('purchase date')
-    paid = models.DecimalField(max_digits=8, decimal_places=2)
+    name = models.CharField(max_length=100)
+    price = models.FloatField(null=True)
+    action = models.CharField(max_length=5, default='buy')
+    quantity = models.IntegerField(default=1)
+    order_date = models.DateField(null=True)
 
     def __str__(self):
-        return "%s, %d" %  (self.symbol, self.shares)
+        return "%s, %s, %s" %  (self.user.username, self.symbol, self.action)
