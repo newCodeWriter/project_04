f3aacc3397a48a343fbf16bc5da3ade3e237f8ec
Shay Brown <fire328@ymail.com>
2020-10-14 17:10:04 -0400

    remove news from home; update watchlist api

--- trade_app/views.py
+++ trade_app/views.py
@@ -23,77 +23,53 @@ from django.http import JsonResponse
 
 def home(request):
 
-    # get news headlines
-    # news_url = "https://bloomberg-market-and-financial-news.p.rapidapi.com/stories/list"
+    # get trending tickers
 
-    # news_query = {"template":"CURRENCY","id":"usdjpy"}
+    trend_url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-trending-tickers"
 
-    # bloom_headers = {
-    #     'x-rapidapi-host': "bloomberg-market-and-financial-news.p.rapidapi.com",
-    #     'x-rapidapi-key': "3031e33fd9msh3c73fd1d3122a19p1a03abjsn0397c5598162"
-    # }
+    trend_query = {"region":"US"}
 
-    # response = requests.request("GET", news_url, headers=bloom_headers, params=news_query)
-    # news_dict = response.json()
-    # headline = news_dict['stories'][0:4]
+    yahoo_headers = {
+        'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com",
+        'x-rapidapi-key': "3031e33fd9msh3c73fd1d3122a19p1a03abjsn0397c5598162"
+    }
 
-    # for h in headline:
-    #     dt = datetime.fromtimestamp(h['published']).strftime("%p %b %Y %H:%M:%S")
-    #     dl = datetime.strptime(dt, "%p %b %Y %H:%M:%S")
-    #     h['published'] = dl
-
-    # updated_now = datetime.now()
-
-    # # get trending tickers
-
-    # trend_url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-trending-tickers"
-
-    # trend_query = {"region":"US"}
-
-    # yahoo_headers = {
-    #     'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com",
-    #     'x-rapidapi-key': "3031e33fd9msh3c73fd1d3122a19p1a03abjsn0397c5598162"
-    # }
-
-    # response = requests.request("GET", trend_url, headers=yahoo_headers, params=trend_query)
-    # trend_dict = response.json()
-    # latest_trends = trend_dict['finance']['result'][0]['quotes'][0:4]
+    response = requests.request("GET", trend_url, headers=yahoo_headers, params=trend_query)
+    trend_dict = response.json()
+    latest_trends = trend_dict['finance']['result'][0]['quotes'][0:4]
 
     # # get popular watchlists
 
-    # pop_url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-popular-watchlists"
+    pop_url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-popular-watchlists"
 
-    # response = requests.request("GET", pop_url, headers=yahoo_headers)
-    # pop_dict = response.json()
-    # pop_watch = pop_dict['finance']['result'][0]['portfolios'][0:3]
+    response = requests.request("GET", pop_url, headers=yahoo_headers)
+    pop_dict = response.json()
+    pop_watch = pop_dict['finance']['result'][0]['portfolios'][0:3]
 
-    # for p in pop_watch:
-    #     p['followerCount'] = "{:,}".format(p['followerCount'])
+    for p in pop_watch:
+        p['followerCount'] = "{:,}".format(p['followerCount'])
     
-    # # get movers
+    # get movers
 
-    # movers_url = "https://morning-star.p.rapidapi.com/market/get-movers"
+    movers_url = "https://morning-star.p.rapidapi.com/market/get-movers"
 
-    # movers_query = {"PerformanceId":"0P00001GJH"}
+    movers_query = {"PerformanceId":"0P00001GJH"}
 
-    # morning_headers = {
-    #     'x-rapidapi-host': "morning-star.p.rapidapi.com",
-    #     'x-rapidapi-key': "3031e33fd9msh3c73fd1d3122a19p1a03abjsn0397c5598162"
-    # }
+    morning_headers = {
+        'x-rapidapi-host': "morning-star.p.rapidapi.com",
+        'x-rapidapi-key': "3031e33fd9msh3c73fd1d3122a19p1a03abjsn0397c5598162"
+    }
 
-    # response = requests.request("GET", movers_url, headers=morning_headers, params=movers_query)
-    # movers_dict = response.json()
-    # actives = movers_dict['Top10']['Actives']['Securities'][0:4]
-    # gainers = movers_dict['Top10']['Gainers']['Securities'][0:4]
+    response = requests.request("GET", movers_url, headers=morning_headers, params=movers_query)
+    movers_dict = response.json()
+    actives = movers_dict['Top10']['Actives']['Securities'][0:4]
+    gainers = movers_dict['Top10']['Gainers']['Securities'][0:4]
 
     context = {
-        # 'news': headline,
-        # 'updated': updated_now,
-        # 'actives': actives,
-        # 'gainers': gainers,
-        # 'trends': latest_trends,
-        'action': 'buy',
-        # 'popular': pop_watch
+        'actives': actives,
+        'gainers': gainers,
+        'trends': latest_trends,
+        'popular': pop_watch
     }
   
     return render(request, 'trade_app/base.html', context)
@@ -145,15 +121,15 @@ def portfolio(request):
         watch_prices = []
 
         for w in watchlist:
-            url = "https://api.twelvedata.com/time_series?symbol=%s&interval=1day&outputsize=2&apikey=441febfb7f41484f965dd7dd447af6b1" % (w['symbol'])
+            url = "https://api.twelvedata.com/quote?symbol=%s&apikey=441febfb7f41484f965dd7dd447af6b1" % (w['symbol'])
             response = requests.request("GET", url)
             data = response.json()
-            close = float(data['values'][0]['close'])
-            last = float(data['values'][1]['close'])
-            change = close - last
-            percent = (change/close)*100
-            high = data['values'][0]['high']
-            low = data['values'][0]['low']
+            close = float(data['close'])
+            last = float(data['previous_close'])
+            change = data['change']
+            percent = data['percent_change']
+            high = data['high']
+            low = data['low']
             info = [w['symbol'], w['name'], close, change, percent, high, low]
             watch_prices.append(info)
         
