5a4bdfaa6f947c4f14e8ecd8d27f575a464bba9c
Shay Brown <fire328@ymail.com>
2020-10-14 00:30:57 -0400

    revise, add urls  and views for plot, trade, etc;

--- trade_app/urls.py
+++ trade_app/urls.py
@@ -6,14 +6,18 @@ from .forms import LoginForm, PasswordChange
 urlpatterns = [
     path('', views.home, name="home"),
     path('portfolio/', views.portfolio, name="portfolio"),
-    path('portfolio/<str:symbol>/<str:name>/', views.add_to_watchlist, name="add-stock"),
-    path('trade/<str:action>/<str:symbol>/', views.trade, name="trade"), 
+    path('portfolio/search/', views.add_to_watch, name="add-stock"),
+    path('portfolio/<str:symbol>/', views.delete_from_watch, name="delete-stock"),
+    path('plot/<str:ticker>/<str:name>', views.get_chart, name="plot"),
+    path('trade/', views.trade, name="trade"), 
+    path('checkout/', views.checkout, name="checkout"), 
     path('login/', LoginView.as_view(template_name='trade_app/login.html', authentication_form=LoginForm), name="login"),
     path('login/user/', views.user_account, name="useract"),
     path('logout/', LogoutView.as_view(template_name='trade_app/logout.html'), name="logout"),
     path('register/', views.get_registration, name="register"),
     path('register/<str:newuser>/confirm/', views.deposit, name="confirm-user"),
+    path('reset/', views.reset, name="reset"),
+    path('reset/confirmed', views.reset_acct, name="reset_acct"),
     path('password_change/', PasswordChangeView.as_view(template_name='trade_app/pwd-change.html', success_url='confirmed', form_class=PasswordChange), name="pwd-change"),
-    path('password_change/confirmed', views.new_pwd_confirmed, name="pwd_confirmed")
-
+    path('password_change/confirmed', views.new_pwd_confirmed, name="pwd_confirmed"),
 ]
--- trade_app/views.py
+++ trade_app/views.py
@@ -1,24 +1,29 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 import requests
+import pandas as pd
+import pandas_datareader as pdr
+from pandas_datareader._utils import RemoteDataError
 import matplotlib.pyplot as plt
-# from matplotlib import style
+from matplotlib.dates import MonthLocator, DateFormatter
+from matplotlib import style
+from django.core.exceptions import ObjectDoesNotExist
+import datetime
+from datetime import date
 from datetime import datetime
 from .forms import RegisterForm, PasswordChange, PasswordReset
 from django.contrib.auth.models import User
-from trade_app.models import Account, Position, Watchlist, Order
+from trade_app.models import Account, StockOrder, Position, Watchlist
 from django.urls import reverse
-from django.contrib.auth.views import LoginView
-
+from django.db.models import F
+import yfinance as yf
+from django.core.cache import cache
+from django.core.cache import caches
+from django.http import JsonResponse
 
 def home(request):
 
-    context = {
-        'symbol': 'AMZN',
-        'action': 'Buy'
-    }    
-    return render(request, 'trade_app/base.html', context)
-    # # get news headlines
+    # get news headlines
     # news_url = "https://bloomberg-market-and-financial-news.p.rapidapi.com/stories/list"
 
     # news_query = {"template":"CURRENCY","id":"usdjpy"}
@@ -33,8 +38,8 @@ def home(request):
     # headline = news_dict['stories'][0:4]
 
     # for h in headline:
-    #     dt = datetime.fromtimestamp(h['published']).strftime("%d %b %Y %H:%M:%S")
-    #     dl = datetime.strptime(dt, "%d %b %Y %H:%M:%S")
+    #     dt = datetime.fromtimestamp(h['published']).strftime("%p %b %Y %H:%M:%S")
+    #     dl = datetime.strptime(dt, "%p %b %Y %H:%M:%S")
     #     h['published'] = dl
 
     # updated_now = datetime.now()
@@ -80,30 +85,125 @@ def home(request):
     # movers_dict = response.json()
     # actives = movers_dict['Top10']['Actives']['Securities'][0:4]
     # gainers = movers_dict['Top10']['Gainers']['Securities'][0:4]
-    # losers = movers_dict['Top10']['Losers']['Securities'][0:4]
 
-    # content = {
-    #     'news': headline,
-    #     'updated': updated_now,
-    #     'actives': actives,
-    #     'gainers': gainers,
-    #     'losers': losers,
-    #     'trends': latest_trends,
-    #     'popular': pop_watch
-    # }
+    context = {
+        # 'news': headline,
+        # 'updated': updated_now,
+        # 'actives': actives,
+        # 'gainers': gainers,
+        # 'trends': latest_trends,
+        'action': 'buy',
+        # 'popular': pop_watch
+    }
+  
+    return render(request, 'trade_app/base.html', context)
 
 def portfolio(request):
-    positions = Position.objects.all().values('symbol', 'shares').filter(user__username=request.user)
-    watchlist = Watchlist.objects.all().values('symbol').filter(user__username=request.user)
+    positions = Position.objects.all().values('symbol', 'name', 'shares').filter(user__username=request.user)
+    watchlist = Watchlist.objects.all().values('symbol', 'name').filter(user__username=request.user)
+
+    if 'symbol' in request.session:
+        symbol = request.session['symbol']
+        name = request.session['name']
+    else: 
+        symbol = None
+        name = None
+
+    # sum total no. of shares
+
     shares = []
-    
     for p in positions:
         shares.append(p['shares'])
-    
     total_shares = sum(shares)
+
+    # create cache for positions and watchlist, API key is limited to 12 requests per minute
+
+    if cache.get('position_list') == None:
+        position_prices = []
+
+        for p in positions:
+            url = "https://api.twelvedata.com/quote?symbol=%s&apikey=441febfb7f41484f965dd7dd447af6b1" % (p['symbol'])
+            response = requests.request("GET", url)
+            data = response.json()
+            close = float(data['close'])
+            last = float(data['previous_close'])
+            change = (close - last) * p['shares']
+            percent = (change/(close * p['shares'])) * 100
+            high = data['high']
+            low = data['low']
+            info = [p['symbol'], p['name'], p['shares'], close, change, percent, high, low]
+            position_prices.append(info)
+
+        if len(position_prices) <= 2:
+            pass
+        else:
+            cache.set('position_list', position_prices, 60)
+    else: 
+        position_prices = cache.get('position_list')
+
+    if cache.get('watch_list') == None:
+        watch_prices = []
+
+        for w in watchlist:
+            url = "https://api.twelvedata.com/time_series?symbol=%s&interval=1day&outputsize=2&apikey=441febfb7f41484f965dd7dd447af6b1" % (w['symbol'])
+            response = requests.request("GET", url)
+            data = response.json()
+            close = float(data['values'][0]['close'])
+            last = float(data['values'][1]['close'])
+            change = close - last
+            percent = (change/close)*100
+            high = data['values'][0]['high']
+            low = data['values'][0]['low']
+            info = [w['symbol'], w['name'], close, change, percent, high, low]
+            watch_prices.append(info)
+        
+        if len(watch_prices) <= 2:
+            pass
+        else:
+            cache.set('watch_list', watch_prices, 60)
+
+    else: 
+        watch_prices = cache.get('watch_list')
+
+    context = {
+        'total': total_shares,
+        'symbol': symbol,
+        'name': name,
+        'positions': position_prices,
+        'watchlist': watch_prices
+    }
+
+    return render(request, 'trade_app/portfolio.html', context)
     
-    if 'search' in request.GET:
-        query = request.GET.get('search')
+def add_to_watch (request):
+    if request.method == 'POST':
+        symbol = request.POST.get('symbol')
+        name = request.POST.get('name')
+        if Watchlist.objects.filter(symbol=symbol).exists():
+            pass
+        else:
+            add = Watchlist(user=request.user, symbol=symbol, name=name)
+            add.save()
+    
+    return HttpResponseRedirect(reverse('portfolio'))
+
+def delete_from_watch (request, symbol):
+    delete = Watchlist.objects.get(symbol=symbol)
+    delete.delete()
+    
+    return HttpResponseRedirect('/portfolio/')
+
+
+def trade(request):
+    
+    cash = Account.objects.values('cash_balance').filter(user__username=request.user)[0]['cash_balance']
+    orders = StockOrder.objects.all().filter(user__username=request.user)
+
+    if 'trade' in request.GET:
+        # get stock info
+
+        query = request.GET.get('trade')
+        
         url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/auto-complete"
         querystring = {"region":"US","q":query}
         headers = {
@@ -111,92 +211,119 @@ def portfolio(request):
             'x-rapidapi-key': "3031e33fd9msh3c73fd1d3122a19p1a03abjsn0397c5598162"
         }
         response = requests.request("GET", url, headers=headers, params=querystring)
-        stock = response.json()
-        symbol = stock['quotes'][0]['symbol'] 
-        name = stock['quotes'][0]['longname'] 
+        data = response.json()
+        symbol = data['quotes'][0]['symbol'] 
+        name = data['quotes'][0]['longname']
     else: 
-        symbol = 'GOOG'
-        name = 'GOOGLE, INC.'
+        symbol = 'AAPL'
+        name = 'Apple, Inc.' 
 
+    # get closing price and historical data for table
+
+    url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-historical-data"
+    querystring = {"region":"US","symbol":symbol}
+    headers = {
+        'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com",
+        'x-rapidapi-key': "3031e33fd9msh3c73fd1d3122a19p1a03abjsn0397c5598162"
+    }
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    data = response.json()
+    close = data['prices'][0]['close']
+    prices = data['prices'][0:7]
+    last = data['prices'][1]['close']
+    change = close - last
+    percent = (change/close) * 100
+
+    for p in prices:
+        dt = datetime.fromtimestamp(p['date']).strftime("%B %d, %Y")
+        p['date'] = dt
+        p['open'] = "${:,.2f}".format(p['open'])
+        p['high'] = "${:,.2f}".format(p['high'])
+        p['low'] = "${:,.2f}".format(p['low'])
+        p['close'] = "${:,.2f}".format(p['close'])
+        p['adjclose'] = "${:,.2f}".format(p['adjclose'])
+
+
+    # loop through users who own shares in the company, and then check if active user already has shares in company 
+
+    users = []
+    positions = Position.objects.values('user').filter(symbol=symbol)
+    for p in positions:
+        users.append(p['user'])
+
+    # if active user has shares in company, get the amount of shares
+
+    if request.user.id in users:
+        share_count = Position.objects.filter(user__username=request.user, symbol=symbol)[0].shares
+    else:
+        share_count = 0
 
     context = {
-        'positions': positions,
-        'watchlist': watchlist,
-        'total': total_shares,
         'symbol': symbol,
+        'cash': cash,
         'name': name,
-        'buy': 'Buy',
-        'sell': 'Sell'
+        'price': close,
+        'table': prices,
+        'orders': orders,
+        'last': last,
+        'users': users,
+        'shares': share_count,
+        'buy': 'buy',
+        'sell': 'sell',
+        'action': 'buy',
+        'change': change,
+        'percent': percent
     }
 
-    return render(request, 'trade_app/portfolio.html', context)
-    
-    # url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-historical-data"
+    return render(request, 'trade_app/trade.html', context)
 
-    # querystring = {"region":"US","symbol":"AMZN"}
-
-    # headers = {
-    #     'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com",
-    #     'x-rapidapi-key': "3031e33fd9msh3c73fd1d3122a19p1a03abjsn0397c5598162"
-    # }
-
-    # response = requests.request("GET", url, headers=headers, params=querystring)
-    # res_dict = response.json()
-    # prices = res_dict['prices'][0:10]
-
-    # # price_list = []
-
-    # for d in prices:
-    #     dt = datetime.fromtimestamp(d['date']).strftime("%m/%d/%Y")
-    #     d['date'] = dt
-    #     d['open'] = "${:,.2f}".format(d['open'])
-    #     d['high'] = "${:,.2f}".format(d['high'])
-    #     d['low'] = "${:,.2f}".format(d['low'])
-    #     d['close'] = "${:,.2f}".format(d['close'])
-    #     d['adjclose'] = "${:,.2f}".format(d['adjclose'])
-    #     d['volume'] = format(d['volume'], ',')
-
-
-    # stock = {
-    #     'prices': prices
-    # }
-
-def add_to_watchlist(request, symbol, name):
+def checkout(request):
     if request.method == 'POST':
-        if Watchlist.objects.filter(symbol=symbol).exists():
-            pass
+        account = Account.objects.filter(user__username=request.user)
+        cash = account[0].cash_balance
+        symbol = request.POST.get('symbol')
+        name = request.POST.get('name')
+        action = request.POST.get('action')
+        quantity = int(request.POST.get('quantity'))
+        total = float(request.POST.get('total'))
+        withdrawal = float(cash) - float(total)
+        deposit = float(cash) + float(total)
+        position = Position.objects.filter(user__username=request.user, symbol=symbol)
+        if action == 'Buy':
+            account.update(cash_balance=withdrawal)
+            if position.exists():
+                position.update(shares=F('shares') + quantity)
+            else:
+                new_position = Position(user=request.user, symbol=symbol, name=name, shares=quantity)
+                new_position.save()
         else:
-            add = Watchlist(user=request.user, symbol=symbol, name=name)
-            add.save()
+            account.update(cash_balance=deposit)
+            if position[0].shares - quantity == 0:
+                position.delete()
+            else:
+                position.update(shares=F('shares') - quantity)
+        # save order
+        order = StockOrder(user=request.user, symbol=symbol, name=name, price=total, action=action, quantity=quantity, order_date=date.today())
+        order.save()
+        return HttpResponseRedirect(reverse('portfolio'))
+    else: 
+        return HttpResponseRedirect(reverse('home'))
+
+def reset(request):
+    return render(request, 'trade_app/reset.html')
+
+def reset_acct(request):
+    Account.objects.filter(user__username=request.user).update(cash_balance=1500.00)
+    Position.objects.filter(user__username=request.user).delete()
+    Watchlist.objects.filter(user__username=request.user).delete()
+    StockOrder.objects.filter(user__username=request.user).delete()
+    # delete any sessions
+    if 'symbol' in request.session:
+        del request.session['symbol']
+        del request.session['name']
     else: 
         pass
-    
-    return HttpResponseRedirect('/portfolio/')
-
-def plotstock(request, stock):
-    stock_data = [stock.date, stock.close]
-    plt.figure(figsize = (12,6))
-    plt.plot(stock_data, 'b')
-    plt.plot(stock_data, 'ro')
-    plt.grid(True)
-    plt.title('Historical Data')
-    plt.xlabel("Date")
-    plt.ylabel("Close Price")
-
-    plt.plot(stock_data)
-    plot_data = plt.show()
-
-    context ={'plot': plot_data}
-        
-    return render(request, 'trade_app/portfolio.html', context)
-
-def trade(request, action='Buy', symbol='AMZN'):
-
-    context = {
-        'symbol': symbol,
-        'action': action
-    }
-    return render(request, 'trade_app/trade.html', context)
+    return HttpResponseRedirect(reverse('home'))
 
 
 # Forms
@@ -231,3 +358,36 @@ def user_account(request):
 
 def new_pwd_confirmed(request):
     return render(request, 'trade_app/new-pwd-confirmed.html')
+
+# get chart
+
+def get_chart(request, ticker, name):
+    # create plot
+    startDate = '2015-01-01'
+    endDate = str(datetime.now().strftime('%Y-%m-%d'))
+    months = MonthLocator(range(1,13), bymonthday=1, interval=3)
+    semi_annual = MonthLocator(range(1,13), bymonthday=1, interval=6)
+    semiFmt = DateFormatter("%b '%y")
+    stock_data = pdr.DataReader(ticker, 'yahoo', startDate, endDate)
+    weekdays = pd.date_range(start=startDate, end=endDate)
+    clean_data = stock_data['Adj Close'].reindex(weekdays)
+    adj_close = clean_data.fillna(method='ffill')
+    fig, ax = plt.subplots()
+    ax.plot(adj_close, 'g')
+    ax.xaxis.set_major_locator(semi_annual)
+    ax.xaxis.set_major_formatter(semiFmt)
+    ax.xaxis.set_minor_locator(months)
+    ax.set_xlabel('Date') 
+    ax.set_ylabel('Adj Close') 
+    plt.grid(True)
+    ax.autoscale_view()
+    fig.autofmt_xdate()
+    plt.savefig('../project_04/trade_app/static/trade_app/images/stock-chart.png')
+    plt.close()
+
+    # create sessions for portfolio to use for trade
+    request.session['symbol'] = ticker
+    request.session['name'] = name
+
+    return HttpResponseRedirect(reverse('portfolio'))
+
